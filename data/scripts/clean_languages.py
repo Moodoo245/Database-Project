e3379188fdb7fb628e80c5b29c538e3bda4c79c2
David Joaquim Da Rocha Rodrigues <david.darocharodrigues@epfl.ch>
2018-05-02 14:47:40 +0100

    add utils.py and exported utils in clean_languages.py

--- data/scripts/clean_languages.py
+++ data/scripts/clean_languages.py
@@ -1,18 +1,7 @@
 import csv
 import unicodedata
 import re
-
-def numbers(idx):
-    return re.sub('[^0-9|]+', '', idx)
-def lettres(idx):
-    return re.sub('[^A-Za-z| ]+', '', idx)
-def alet(idx):
-    return re.sub('[^0-9A-Za-z| ]+', '', idx)
-def acc(idx):
-    nfkd_form = unicodedata.normalize('NFKD', idx)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
-def diff_letters(a,b):
-    return abs(len(a) - len(b))
+import utils
 
 with open('initial_data/languages.csv', encoding="utf8") as csvfile:
     reader = csv.reader(csvfile)
---
+++ data/scripts/utils.py
@@ -0,0 +1,15 @@
+import csv
+import unicodedata
+import re
+
+def numbers(idx):
+    return re.sub('[^0-9|]+', '', idx)
+def lettres(idx):
+    return re.sub('[^A-Za-z| ]+', '', idx)
+def alet(idx):
+    return re.sub('[^0-9A-Za-z| ]+', '', idx)
+def acc(idx):
+    nfkd_form = unicodedata.normalize('NFKD', idx)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+def diff_letters(a,b):
+    return abs(len(a) - len(b))
