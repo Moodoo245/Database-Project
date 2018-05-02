c9742144a9c3bac625bd78f1ca4daac6a51da6ea
David Joaquim Da Rocha Rodrigues <david.darocharodrigues@epfl.ch>
2018-05-02 16:27:25 +0100

    add clean_clips

---
+++ data/scripts/clean_clips.py
@@ -0,0 +1,19 @@
+import csv
+import unicodedata
+import re
+import utils
+
+with open('initial/clips.csv', encoding="utf8") as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    with open('cleaned/clips_cleaned.csv', 'w', encoding="utf8") as out:
+        wr = csv.writer(out)
+        added = set()
+        for row in reader:
+            clipid = row[0]
+            # Only keep the numbers in the "Year" column
+            only_numbers = utils.numbers(row[2])
+            # Only keep the doubles in the "ClipType" column
+            only_letters = utils.lettres(row[3])
+            wr.writerow((clipid, row[1], only_numbers, only_letters))
+            added.add((clipid))
