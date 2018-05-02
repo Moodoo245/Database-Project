200d2107cd3cef77cf5bdca1ca85ddd507b5f294
David Joaquim Da Rocha Rodrigues <david.darocharodrigues@epfl.ch>
2018-05-02 14:49:10 +0100

    Corrected paths in clean_languages.py

--- data/scripts/clean_languages.py
+++ data/scripts/clean_languages.py
@@ -3,10 +3,10 @@ import unicodedata
 import re
 import utils
 
-with open('initial_data/languages.csv', encoding="utf8") as csvfile:
+with open('initial/languages.csv', encoding="utf8") as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
-    with open('cleaned_data/languages_cleaned.csv', 'w', encoding="utf8") as out:
+    with open('cleaned/languages_cleaned.csv', 'w', encoding="utf8") as out:
         wr = csv.writer(out)
         added = set()
         for row in reader:
