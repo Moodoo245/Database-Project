d328b0acf8e41414954739c0d6decb88ae4769d9
Moodoo245 <justinas.sukaitis@epfl.ch>
2018-05-17 14:52:41 +0200

    forgot to save c:

--- data/scripts/clean_countries.py
+++ data/scripts/clean_countries.py
@@ -2,24 +2,7 @@ import csv
 import unicodedata
 import re
 import utils
-'''
-/////////////OLD VERSION/////////////////
-with open('initial/countries.csv', encoding="utf8") as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader)
-    with open('cleaned/countries_cleaned.csv', 'w', encoding="utf8") as out:
-        wr = csv.writer(out)
-        added = set()
-        for row in reader:
-            clipid = row[0]
-            l = utils.acc(row[1])
-            b = utils.lettres(l)
-            new_row = (clipid, b)
-            if utils.diff_letters(l, b) < 2 and len(b) != 0 and b.lower() != 'null' and new_row not in added:
-                wr.writerow(new_row)
-                added.add(new_row)
-/////////////OLD VERSION/////////////////
-'''
+
 with open('cleaned/countries_cleaned.csv', 'w', encoding='utf8') as fout, \
     open('cleaned/country_map.csv', 'w', encoding='utf8') as fcountry_map:
     out = csv.writer(fout)
