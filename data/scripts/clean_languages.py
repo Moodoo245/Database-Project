913c79fa6926a47e94afcb358cb38e5e77aa5f54
David Joaquim Da Rocha Rodrigues <david.darocharodrigues@epfl.ch>
2018-05-17 15:07:52 +0200

    implement scripts to create the .csv of genres, classified, languages and speaks

--- data/scripts/clean_languages.py
+++ data/scripts/clean_languages.py
@@ -3,6 +3,10 @@ import unicodedata
 import re
 import utils
 
+languages = set()
+languageId = 1
+language_map = {}
+
 with open('initial/languages.csv', encoding="utf8") as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
@@ -11,9 +15,36 @@ with open('initial/languages.csv', encoding="utf8") as csvfile:
         added = set()
         for row in reader:
             clipid = row[0]
-            l = utils.acc(row[1])
+            language = row[1]
+            l = utils.acc(language)
             b = utils.lettres(l)
-            new_row = (clipid, b)
-            if utils.diff_letters(l, b) < 2 and len(b) != 0 and b.lower() != 'null' and new_row not in added:
+
+            if utils.diff_letters(l, b) < 2 and len(b) != 0 and b.lower() != 'null' and b.lower() != 'none':
+                if b not in languages:
+                    new_row = (languageId, b)
+                    if new_row not in added:
+                        languages.add(b)
+                        language_map[b] = languageId
+                        languageId += 1
+                        wr.writerow(new_row)
+                        added.add(new_row)
+                else:
+                    new_row = (language_map[b], b) 
+                    if new_row not in added:
+                        wr.writerow(new_row)
+                        added.add(new_row)
+
+with open('initial/languages.csv', encoding="utf8") as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    with open('cleaned/speaks_cleaned.csv', 'w', encoding="utf8") as out:
+        wr = csv.writer(out)
+        added = set()
+        for row in reader:
+            clipid = row[0]
+            language = row[1]
+            l = utils.acc(language)
+            b = utils.lettres(l)
+            if b in languages:
+                new_row = (clipid, language_map[b])
                 wr.writerow(new_row)
-                added.add(new_row)
