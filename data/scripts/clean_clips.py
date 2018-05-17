682160e0a66f2774b22118b42171a955a4537b9e
Justinas Sukaitis <justinas.sukaitis>
2018-05-17 16:17:00 +0200

    clean clips works

--- data/scripts/clean_clips.py
+++ data/scripts/clean_clips.py
@@ -4,25 +4,24 @@ import re
 import utils
 
 with open('initial/clips.csv', encoding='utf8') as csvfile:
-	
     reader = csv.reader(csvfile)
     next(reader)
-
-    with open('cleaned/clips_cleaned.csv', 'w', encoding='utf8') as out:
-        wr = csv.writer(out)
-        added = set()
-        for row in reader:
-            clipid = row[0]
-            # Only keep the numbers in the "Year" column
-            only_numbers = utils.numbers(row[2])
-            # Only keep the doubles in the "ClipType" column
-            only_letters = utils.lettres(row[3])
-            if len(clipid) != 0 and clipid.lower() != 'null' and (clipid) not in added:
-                wr.writerow((clipid, row[1], only_numbers, only_letters))
-                added.add((clipid))
-		
-		
-with open('cleaned/clips_set.csv', 'w', encoding='utf8') as fclips_set:
-
-	clip_set = csv.writer(fclips_set)
-	clip_set.writerows(added)
+    with open('cleaned/clips_set.csv', 'w', encoding='utf8') as fclips_set:
+        clip_set = csv.writer(fclips_set)
+        with open('cleaned/clips_cleaned.csv', 'w', encoding='utf8') as out:
+            wr = csv.writer(out)
+            added = set()
+            for row in reader:
+                clipid = row[0]
+                # Only keep the numbers in the "Year" column
+                only_numbers = utils.numbers(row[2])
+                # Only keep the doubles in the "ClipType" column
+                only_letters = utils.lettres(row[3])
+                if len(clipid) != 0 and clipid.lower() != 'null' and (clipid) not in added:
+                    wr.writerow((clipid, row[1], only_numbers, only_letters))
+                    added.add((clipid))
+                    clip_set.writerow([clipid])
+                    '''
+                    for id in added:
+                        clip_set.writerow(id)
+                        '''
